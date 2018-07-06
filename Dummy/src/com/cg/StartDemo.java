c9e891c9f567f77cf0ca62d81613b65e2ac791e8
apatni <apatni@DIN66007373.corp.capgemini.com>
2018-07-06 17:48:46 +0530

    Updated code

--- Dummy/src/com/cg/StartDemo.java
+++ Dummy/src/com/cg/StartDemo.java
@@ -6,6 +6,7 @@ public class StartDemo {
 		// TODO Auto-generated method stub
              System.out.println("hello");
             System.out.println("Github");
+            System.out.println("welcome");
 	}
 
 }
